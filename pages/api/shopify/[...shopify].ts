1dd9883196650a2d9a767cd6dc7ebddd6c9ec7d5
shubham79a <shubh79.nolan@gmail.com>
2025-06-18 15:59:23 +0530

    fixes types

--- pages/api/shopify/[...shopify].ts
+++ pages/api/shopify/[...shopify].ts
@@ -1,10 +1,17 @@
 // pages/api/auth/[...shopify].ts
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { shopify } from '../shopify';
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === 'GET') {
     const { shop } = req.query;
-    if (typeof shop !== 'string') return res.status(400).send('Missing shop');
+
+    if (typeof shop !== 'string') {
+      return res.status(400).send('Missing shop');
+    }
 
     const redirectUrl = await shopify.auth.begin({
       shop,
